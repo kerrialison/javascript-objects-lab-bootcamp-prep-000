22e77777c1d4ec4b5efa136bc08d0651c31eb25b
Kerri Varner <kerrivarner@gmail.com>
2018-11-14 18:23:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object){
-  recipes.eggs = "white omelettes",
-  recipes.snacks = "sesame crisp rounds",
-  recipes.salads = "mediterranean heaven";
-  return object;
-}
-
 function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign({}, obj, { [key]: value })
   return object
