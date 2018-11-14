a0b6a696fb9447773f9c61e0c08f83eacd873887
Kerri Varner <kerrivarner@gmail.com>
2018-11-14 16:59:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var recipes = {}
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  recipes[breakfast] = "white omelettes";
+  recipes[lunch] = "asian vegetable wraps";
+  recipes[dinner] = "NY strip steak and garlic mashed potatoes";
+
+}
